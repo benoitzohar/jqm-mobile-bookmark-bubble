e7622ee47e43ae4056f69b09f1ace2d71f342b89
Benoit Zohar <benoit.zohar@versusmind.eu>
2015-03-28 14:22:17 +0100

    French Translation
    Added/improved french translations for android44 & ios7orlater

--- bookmark_bubble_msg_fr.js
+++ bookmark_bubble_msg_fr.js
@@ -7,14 +7,16 @@ google.bookmarkbubble.Bubble.prototype.msg = {
     '<b>Installer cette app:</b><br /> 1) Appuyez sur <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_ANDROID4_MOBILE_BOOKMARK_DATA_URL_ +'" style="height: 1.5em;display: inline-block;padding:0;margin:0;" />,<br /> 2) Select "<b>Sauver dans les favoris</b>",<br /> 3) Choisir "<b>Ajouter à</b>" et "<b>Ecran d\'accueil</b>"',
   android41:
     '<b>Installer cette app:</b><br /> 1) Ajouter un favori,<br /> 2) Appuyez et maintenez le favori,<br /> 3) Select "<b>Ajouter raccourci écran d\'accueil</b>"',
-  android44:/** TODO: Please translate this messsage in French!! **/
-    '<b>Install this app:</b><br /> 1) Tap <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_ANDROID4_MOBILE_BOOKMARK_DATA_URL_ +'" style="height: 1.5em;display: inline-block;padding:0;margin:0;" />,<br /> 2) Select "<b>Add to homescreen</b>"<br /><br />',
+  android44:
+    '<b>Installer cette app:</b><br /> 1) Appuyez sur <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_ANDROID4_MOBILE_BOOKMARK_DATA_URL_ +'" style="height: 1.5em;display: inline-block;padding:0;margin:0;" />,<br /> 2) Choisissez "<b>Ajouter à l\'écran d\'accueil</b>"<br /><br />',
   blackberry: 
-    '<b>Installer cette app:</b><br /> Appuyez sur <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_BLACKBERRY_ICON_DATA_URL_ +'" style="height: 1em;display: inline-block;padding:0;margin:0" />, choisir "<b>Ajouter à l\'écran d\'accueil</b>"',
+    '<b>Installer cette app:</b><br /> Appuyez sur <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_BLACKBERRY_ICON_DATA_URL_ +'" style="height: 1em;display: inline-block;padding:0;margin:0" />, choisissez "<b>Ajouter à l\'écran d\'accueil</b>"',
   playbook: 
-     '<b>Installer cette app:</b><br /> Appuyez sur <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_PLAYBOOK_BOOKMARK_DATA_URL_ +'" style="height: 1.5em;display: inline-block;padding:0;margin:0;" />, choisir  <br />"<b>Ajouter à l\'écran d\'accueil</b>"',
+     '<b>Installer cette app:</b><br /> Appuyez sur <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_PLAYBOOK_BOOKMARK_DATA_URL_ +'" style="height: 1.5em;display: inline-block;padding:0;margin:0;" />, choisissez  <br />"<b>Ajouter à l\'écran d\'accueil</b>"',
   ios42orlater :
      '<b>Installer cette app:</b><br /> Appuyez sur <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_SAFARI_FORWARD_DATA_URL_ +'" style="height: 1em;display: inline-block;padding: 0;margin: 0" /> puis <b>"Ajouter à l\'écran d\'accueil"</b>',
+  ios7orlater :
+     '<b>Installer cette app</b>:<br /> Appuyez sur <img class="bookmark-bubble-menu-icon" src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_IOS7_FORWARD_DATA_URL_ +'" style="height: 2em;display: inline-block;padding: 0;margin: 0" /> puis <b>"Ajouter à l\'écran d\'accueil"</b>',
   ioslegacy:
      '<b>Installer cette app:</b><br /> Appuyez sur <b style="font-size:15px">+</b> et <b>"Ajouter à l\'écran d\'accueil"</b>'
 };
